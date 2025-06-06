7c2300e7988ad57c1254b6fd75c55bd3411fe75e
Mateusz Furgała <74820050+matifur@users.noreply.github.com>
2025-06-06 17:36:51 +0200

    Update ble_server.h

--- BLE/ble_server.h
+++ BLE/ble_server.h
@@ -1,12 +1,13 @@
 #pragma once
-#include <stdint.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void ble_server_init(void);
-void notify_speed(float kmh);          // łatwo dodać kolejne notify_xyz()
+
+void notify_speed(float kmh);
+void notify_avg_speed(float kmh);
+void notify_distance(float km);
 
 #ifdef __cplusplus
 }
